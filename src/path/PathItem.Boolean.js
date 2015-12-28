593ab9e9a787966cff0f451a19f91a00a3282d55
Jürg Lehni <juerg@scratchdisk.com>
2015-12-28 16:37:54 +0100

    Revert back to always call #setClockwise() in #resolveCrossings()
    This solves some regressions, e.g. https://github.com/paperjs/paper.js/issues/869#issuecomment-167586249

--- src/path/PathItem.Boolean.js
+++ src/path/PathItem.Boolean.js
@@ -1031,6 +1031,7 @@ PathItem.inject(new function() {
                 });
                 var items = [],
                     excluded = {},
+                    clockwise = paths[0].isClockwise(),
                     isNonZero = this.getFillRule() === 'nonzero',
                     windings = isNonZero && Base.each(paths, function(path) {
                         this.push(path.isClockwise() ? 1 : -1);
@@ -1063,12 +1064,13 @@ PathItem.inject(new function() {
                             isContained = true;
                             if (!excluded[j]) {
                                 // Set opposite orientation of containing path.
-                                path.setClockwise(!paths[j].isClockwise());
+                                clockwise = !paths[j].isClockwise();
                                 break;
                             }
                         }
                     }
                     if (!exclude) {
+                        path.setClockwise(clockwise);
                         items.push(path);
                     }
                 }
